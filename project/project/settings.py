142e549f9e989e2269b581d118d5f6613a132cec
rajke24 <jarek.andrzejewski99@gmail.com>
2020-11-03 17:54:28 +0100

    set database to MongoDB

--- project/project/settings.py
+++ project/project/settings.py
@@ -74,13 +74,21 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'library_database',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
