4b8af1749fe05f0ac01b7ba8fe531ac94389c36b
rajke24 <jarek.andrzejewski99@gmail.com>
2020-11-13 15:18:24 +0100

    Added register view

--- project/users/views.py
+++ project/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
 
-# Create your views here.
+# User registration view
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            usernames = form.cleaned_data['username']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            return redirect(request, 'landing-home')
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'users/registers.html', {'form': form})
+    
