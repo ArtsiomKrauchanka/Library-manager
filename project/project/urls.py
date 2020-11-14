0e66132ade2c1db3547e43754256f133c0f1cde4
rajke24 <jarek.andrzejewski99@gmail.com>
2020-11-14 12:40:32 +0100

    Added signup template

--- project/project/urls.py
+++ project/project/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from landing import views as landing_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', landing_views.home, name='landing-home'),
+    path('user/', include('users.urls')),
 ]
