f683d0a3f5ecc8f4b8cff881e5d471048322d7f2
Vladyslav Shpychko <58635862+vlad-shp@users.noreply.github.com>
2020-11-14 16:46:53 +0200

    Created books app

--- project/project/settings.py
+++ project/project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'landing.apps.LandingConfig',
     'users.apps.UsersConfig',
+    'books.apps.BooksConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
