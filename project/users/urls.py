0ea94b52c38dc8f1d2c76669a662a340e7870902
rajke24 <jarek.andrzejewski99@gmail.com>
2020-11-16 12:09:06 +0100

    Added reset password template

--- project/users/urls.py
+++ project/users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name="users/password_reset_form.html"), name='password_reset_form.html')
 ]
