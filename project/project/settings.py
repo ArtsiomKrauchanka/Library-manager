7c5b6b846cbb0825696ed597dc860d72c078b674
rajke24 <jarek.andrzejewski99@gmail.com>
2020-11-16 15:48:20 +0100

    Added email settings

--- project/project/settings.py
+++ project/project/settings.py
@@ -138,3 +138,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = 'landing-home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('GMAIL')
+EMAIL_HOST_PASSWORD = os.getenv('GMAIL_APP_PASSWORD')
