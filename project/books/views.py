1e548cd6a05ddc3fc7084d725d99a6c8ba45db02
Vladyslav Shpychko <58635862+vlad-shp@users.noreply.github.com>
2020-11-17 17:42:08 +0200

    Hided "About us" on library page

--- project/books/views.py
+++ project/books/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render, redirect
 
 def library(request):
-    return render(request, 'books/library.html', {'title': 'Your library'})
+    return render(request, 'books/library.html', {'title': 'Your library','library_selcted_class':'underline-it', 'show_extras':"no"})
