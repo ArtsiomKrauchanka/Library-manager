0279b6803308e1aa58ba7423ff3ea1caa37c5acd
Vladyslav Shpychko <58635862+vlad-shp@users.noreply.github.com>
2020-11-21 14:50:57 +0200

    Library template was changed

--- project/books/views.py
+++ project/books/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render, redirect
 
 def library(request):
-    return render(request, 'books/library.html', {'title': 'Your library','library_selcted_class':'nav-selected', 'show_extras':"no"})
+    return render(request, 'books/library.html', {'title': 'Your library','library_class':'nav-selected', 'show_extras':"no"})
