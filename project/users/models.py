c40226709d8fe199697642bccd9c547db8a8750a
rajke24 <jarek.andrzejewski99@gmail.com>
2020-11-22 14:11:30 +0100

    Update profile model

--- project/users/models.py
+++ project/users/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    favourite_book = models.CharField(max_length=150)
+    favourite_genre = models.CharField(max_length=150)
+    mobile_phone = models.CharField(max_length=9)
 
     def __str__(self):
         return f'{self.user.username} Profile'
