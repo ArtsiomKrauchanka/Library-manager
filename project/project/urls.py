f41c39c9f806abb61b0e1df9f797e769038c2882
rajke24 <jarek.andrzejewski99@gmail.com>
2020-11-25 13:01:32 +0100

    Add new login view

--- project/project/urls.py
+++ project/project/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
+from django.views.generic.base import RedirectView
 from landing import views as landing_views
 
 urlpatterns = [
+    path('admin_panel/', RedirectView.as_view(url=reverse_lazy('admin:index')), name='admin'),
     path('admin/', admin.site.urls),
     path('', landing_views.home, name='landing-home'),
     path('user/', include('users.urls')),
--- project/users/urls.py
+++ project/users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import register, profile, profile_edit
+from .views import register, login_page, profile, profile_edit
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html',redirect_authenticated_user=True), name='login'),
+    path('login/', login_page, name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path(
         'password-reset/', 
