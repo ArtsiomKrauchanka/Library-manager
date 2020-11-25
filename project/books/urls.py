5529f70d03408855bddb0bcee52b0079b51f7a1a
rajke24 <jarek.andrzejewski99@gmail.com>
2020-11-25 22:18:27 +0100

    Add book filter by genre

--- project/books/urls.py
+++ project/books/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import library, bookDetails, book_search_view
+from .views import library, bookDetails, book_filter_view
 
 urlpatterns = [
     path('library/', library, name='library'),
-    path('library/search-book/', book_search_view, name='search_book'),
+    path('library/search-book/', book_filter_view, name='filter_book'),
     path('book_details/<uuid:pk>/', bookDetails, name='bookDetails'),
 ]
--- project/books/views.py
+++ project/books/views.py
@@ -1,31 +1,52 @@
 from django.shortcuts import render, redirect
-from .models import Book, Author
+from .models import Book, Author, Genre
 from django.shortcuts import render, get_object_or_404
 
 
 def library(request):
     bookList = Book.objects.all().order_by('title')
-    return render(request, 'books/library.html', {'title': 'Your library', 'library_class': 'nav-selected',
-                                                  'show_extras': "no", 'bookList': bookList})
-
-
+    genres = Genre.objects.all().order_by('name')
+    
+    context = {
+        'title': 'Your library',
+        'library_class': 'nav-selected',
+        'show_extras': 'no', 
+        'bookList': bookList,
+        'genres': genres,
+    }
+    return render(request, 'books/library.html', context)
 
 def bookDetails(request, pk):
     book = get_object_or_404(Book, id=pk)
     return render(request, 'books/book_details.html', {'title': 'Book details', 'book': book})
 
-def book_search_view(request):
+def is_valid_queryparam(param):
+    return param != '' and param is not None
+
+def book_filter_view(request):
     qs = Book.objects.all()
     book_title = request.GET.get('book_title')
     book_author = request.GET.get('book_author')
+    book_genre = request.GET.get('book_genre')
+    
+    genres = Genre.objects.all().order_by('name')
+    searched = False
 
-    if book_title != '' and book_title is not None:
+    if is_valid_queryparam(book_title):
         qs = qs.filter(title__icontains=book_title).order_by('title')
-    elif book_author != '' and book_author is not None:
+        searched = True
+    elif is_valid_queryparam(book_author):
         qs = qs.filter(author__last_name__icontains=book_author) | qs.filter(author__first_name__icontains=book_author)
         qs = qs.order_by('title')
+        searched = True
+    if is_valid_queryparam(book_genre):
+        qs = qs.filter(genre__name=book_genre)
+    
     context = {
-        'bookList': qs
+        'bookList': qs,
+        'genres': genres,
+        'searched': searched,
+        'genre': book_genre,
     }
 
     return render(request, 'books/library.html', context)
