ddfbf5848eda2a01407c9f4ac148a9de2f8ad36a
rajke24 <jarek.andrzejewski99@gmail.com>
2020-11-30 16:43:21 +0100

    Update admin panel

--- project/books/admin.py
+++ project/books/admin.py
@@ -4,12 +4,16 @@ from .models import Genre, Author, Book, BookInstance, Opinion
 
 admin.site.register(Genre)
 admin.site.register(Author)
-admin.site.register(BookInstance)
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ['id','title', 'author']
     search_fields = ['title', 'author__last_name', 'author__first_name']
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'book']
+    search_fields = ['id', 'book__title', 'book__author__first_name', 'book__author__last_name']
 
 @admin.register(Opinion)
 class OpinionAdmin(admin.ModelAdmin):
