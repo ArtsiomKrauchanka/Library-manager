8052492b7fa7d64a11496c31915804bc01855d44
ArtsiomKrauchanka <ArtsiomKrauchanka@users.noreply.github.com>
2021-01-11 03:01:09 +0100

    changed to postgresql

--- project/project/settings.py
+++ project/project/settings.py
@@ -85,19 +85,33 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-'''
+
+#DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'djongo',
+#        'NAME': 'library_database',
+#    }
+#}
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'library_database',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'password',
+
+        'HOST': '127.0.0.1',
+
+        'PORT': '5432',
     }
 }
 
